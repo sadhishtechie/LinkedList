c0cf13f43d4ab09b07c34debb4eff680ac7fba9e
sadhishtechie <67589226+sadhishtechie@users.noreply.github.com>
2020-12-08 22:05:33 +0530

    LinkedList
    LinkedList

---
+++ List/src/Runner.java
@@ -0,0 +1,17 @@
+//import java.util.*;
+//import java.util.LinkedList;
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList list = new LinkedList();
+		list.insert(5);
+		list.insert(20);
+		list.insert(58);
+		list.show();
+		//System.out.println(list.size());
+		//System.out.println(list);
+
+	}
+
+}
